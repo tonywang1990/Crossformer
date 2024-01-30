6aaf2a72f2ef9a8ebe74e40970d0a0f4d9135fd7
Tony Wang <tonywy@meta.com>
2024-01-29 21:10:41 -0600

    add training script

---
+++ train.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# DEBUG ONLY: Overfitting 1 day of data
+python main_crossformer.py --data futs --root_path /Users/tonywy/Desktop/Xode/crossformer --train_path futs_data/ZCE_CH_UR/daily_frame.20231211.parquet --val_path futs_data/ZCE_CH_UR/daily_frame.20231211.parquet --in_len 1024 --out_len 4 --data_dim 24 --d_model 64 --d_ff 128 --batch_size 32 --train_epoch 20 --save_pred 
